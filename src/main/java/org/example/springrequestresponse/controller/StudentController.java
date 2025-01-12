4fd709caf2ad73840c792c0c3c3b45f693dec942
justk <justkukaro@naver.com>
2025-01-12 17:22:31 +0900

    feat:add

--- src/main/java/org/example/springrequestresponse/controller/StudentController.java
+++ src/main/java/org/example/springrequestresponse/controller/StudentController.java
@@ -25,7 +25,12 @@ public class StudentController {
     }
 
     @GetMapping("/grade/{grade}")
-    public ApiResponse<Student> searchStudentByGrade(@PathVariable String grade) {
+    /*
+     * 스프링부트 3.2버전부터 PathVariable에 name을 생략하지 않으면 안되도록 변경됨
+     * @PathVariable("grade") -> 가능
+     * @PathVariable(name="grade") -> 가능
+     */
+    public ApiResponse<Student> searchStudentByGrade(@PathVariable("grade") String grade) {
         List<Student> result = studentService.getByGrade(grade);
         return makeResponse(result);
     }
